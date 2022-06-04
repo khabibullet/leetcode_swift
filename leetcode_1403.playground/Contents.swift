387f1bb80fe689d016704fe3af06570e4c1db1de
khabibullet <irek.rifson@gmail.com>
2022-06-04 16:48:58 +0300

    Method 2

--- leetcode_1403.playground/Contents.swift
+++ leetcode_1403.playground/Contents.swift
@@ -1,5 +1,6 @@
 // 1403. Minimum Subsequence in Non-Increasing Order
 
+// Method 1 (Faster)
 class Solution {
     func minSubsequence(_ nums: [Int]) -> [Int] {
         
@@ -19,6 +20,28 @@ class Solution {
     }
 }
 
-var test = Solution()
+// Method 2
 
-print(test.minSubsequence([1]))
+class Solution2 {
+    func minSubsequence(_ nums: [Int]) -> [Int] {
+        
+        let seq = nums.sorted(by: { $0 >= $1 }).map { $0 }
+        var res = [Int]()
+        var seqSum = seq.reduce(0, +)
+        var resSum = 0
+        
+        for num in seq {
+            if (resSum > seqSum) {
+                return res
+            }
+            res.append(num)
+            seqSum -= num
+        	resSum += num
+        }
+        return res
+    }
+}
+
+var test = Solution2()
+
+print(test.minSubsequence([4,3,10,9,8]))
